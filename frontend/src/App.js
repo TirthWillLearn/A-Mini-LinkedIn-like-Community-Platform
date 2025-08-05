c283d40053e1e7abe45f86965b3007187741e64e
Tirth Patel <111308497+TirthWillLearn@users.noreply.github.com>
2025-08-05 23:06:31 +0530

    Add: added app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
